e8f668a6e8060bb458892ab05b71535053bb6ce0
kaikyou76 <kaikyou@gmail.com>
2024-07-20 16:51:56 +0800

    【Request Method】GETとPOST

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -5,12 +5,42 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const users = await prisma.user.findMany({
-    select: {
-        id: true,
-        name: true,
-        email: true
-    },
-  })
-  res.status(200).json(users)
+
+  const {method, body} = req//メソッドがreqに格納されています。
+
+  switch (method) {
+    case 'GET':
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true
+            },
+          })
+          res.status(200).json(users)
+      break
+
+    case 'POST':
+　　　　const createdUser = await prisma.user.create({
+             data: {
+                email: body.email,
+                name: body.name,
+                password: body.password
+             },
+             select: {
+                id: true,
+                name: true,
+                email: true
+             },
+        })
+        res.status(200).json(createdUser)
+      break     
+
+    default:
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`${method} method not allowed`)
+  }
+
+
+
 }
