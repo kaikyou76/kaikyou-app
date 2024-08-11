49e0dd186c0c1d70f58e0254d11ce0f61b7e9109
kaikyou76 <kaikyou@gmail.com>
2024-08-11 21:01:12 +0800

    016【UserDetails Page１】

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Card } from 'flowbite-react'
 import { CheckBadgeIcon } from '@heroicons/react/24/solid'
 import { User } from '@prisma/client'
+import Link from 'next/link'
 
 interface Props {
     user: Pick<User, 'id'|'email'|'name'>
@@ -19,12 +20,12 @@ const UserCard = ({user}: Props) => {
       <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{user.name}</h5>
       <span className="text-sm text-gray-500 dark:text-gray-400">{user.email}</span>
       <div className="mt-4 flex space-x-3 lg:mt-6">
-        <a
-          href="#"
+        <Link
+          href={`/users/${user.id}`}
           className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
         >
-          Add friend
-        </a>
+          User Details
+        </Link>
       </div>
     </div>
   </Card>
